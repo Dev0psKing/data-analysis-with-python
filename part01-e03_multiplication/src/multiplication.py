4ba7986c3e0a04fa68048bca477ce60b0e48b23c
Uwabor, Collins <legendarycolins@yahoo.com>
2025-01-14 18:25:44 +0100

    updated

--- part01-e03_multiplication/src/multiplication.py
+++ part01-e03_multiplication/src/multiplication.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def main():
-    # Enter your solution here
+     
+     for i in range(11):  # Loop from 0 to 10
+        print(f"4 multiplied by {i} is {4 * i}")
 
 if __name__ == "__main__":
     main()
