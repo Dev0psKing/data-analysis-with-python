4d0523c50928972426fc91bc7c89ce9e42cc3049
Uwabor, Collins <legendarycolins@yahoo.com>
2025-01-17 18:53:21 +0100

    updated

--- part01-e01_hello_world/src/hello_world.py
+++ part01-e01_hello_world/src/hello_world.py
@@ -13,6 +13,7 @@ def main():
     print("Hello, world!")
     # Enter your code here, this is the correct indentation
 
+
 # This makes sure the main function is not called immediatedly
 # when TMC imports this module
 if __name__ == "__main__": 
