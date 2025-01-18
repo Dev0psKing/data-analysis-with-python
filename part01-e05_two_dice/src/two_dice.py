b4ea309e872353a7e6171487b431f5e71590fd1d
Uwabor, Collins <legendarycolins@yahoo.com>
2025-01-18 17:50:01 +0100

    updated

--- part01-e05_two_dice/src/two_dice.py
+++ part01-e05_two_dice/src/two_dice.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def main():
-    pass
+   
+    for i in range(1, 7):  # Values for the first die (1 to 6)
+        for j in range(1, 7):  # Values for the second die (1 to 6)
+            if i + j == 5:
+                print(f"({i},{j})")  # Removed the space before the comma
 
 if __name__ == "__main__":
     main()
